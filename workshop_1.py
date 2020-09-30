e2375461bdd18fb233cfea2b8a5b293b6c2ae31e
Shawn Duong <shawnduong@protonmail.com>
2020-09-30 14:30:02 -0700

    Added workshop_1.py.

---
+++ workshop_1.py
@@ -0,0 +1,50 @@
+# 2020 Python Programming Workshop - Day 1: Rock, Paper, Scissors!
+# Made possible by Engineering Service Learning and the Solar Energy Association
+
+# Welcome to the first Python programming workshop! This may be an exciting
+# experience for you, especially if you've never programmed before. If you have
+# any trouble at any time, then please ask for help.
+
+# Prompt: create a "Rock, Paper, Scissors!" game in Python.
+# These comments and some pre-written code will help you make your program.
+
+
+
+# You don't need to worry about what this line of code does (for now). We will
+# learn more about this in the future! Feel free to ignore it.
+
+from lib.rock_paper_scissors import *
+
+
+
+# When the player starts your program, they should be greeted with some kind of
+# a welcoming message. Put that message here. Can you remember how we output
+# data to the terminal?
+
+
+
+# Now that we've greeted the player to our game, let's get their decision! We
+# will ask them if they want to choose rock, paper, or scissors, and then we
+# will get their input. Can you remember how we prompt and get input from the
+# user?
+
+
+
+# Now that we've gotten the user's decision, the computer needs to make a
+# decision too. There is a special function I've made for you called
+# computer_choice() that will make a choice for the computer. You can use this
+# function I've made or you can try doing it yourself. You'll also want to
+# output what they chose so that the user knows.
+
+
+
+# Now that we have the user's choice ("rock", "paper", or "scissors") and we
+# have the computer's choice ("rock", "paper", or "scissors"), then we can
+# start computing the winner of the game! Can you think of a way to make
+# comparisons and then execute certain code based off of that?
+
+
+
+# That's all there is to making a rock paper scissors game! You should be very
+# proud of yourself at this point if this was your first time programming.
+# Now run your program, fix any bugs, and play a few rounds!
